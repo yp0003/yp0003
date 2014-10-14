a3bb1b444f4f9c0a4a0cb1a4fe12d26cf3e34bcd
767175432@qq.com <Simon@My-PC>
2014-10-14 20:17:26 +0800

    增加购物车POJO和修正数据库连接信息

---
+++ YP0003/src/main/java/com/yfann/web/action/sys/OrderAction.java
@@ -0,0 +1,18 @@
+package com.yfann.web.action.sys;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OrderAction extends CommonAction{
+
+	final Logger logger = LoggerFactory.getLogger(OrderAction.class);
+	private static final long serialVersionUID = 5350272860785771007L;
+	/**
+	 * 添加到购物车
+	 * @return
+	 */
+	public String addBuyCar() {
+		return "addBuyCar";
+	}
+
+}
---
+++ YP0003/src/main/java/com/yfann/web/service/impl/OrderServiceImpl.java
@@ -0,0 +1,9 @@
+package com.yfann.web.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.yfann.web.service.OrderService;
+@Service
+public class OrderServiceImpl implements OrderService{
+
+}
